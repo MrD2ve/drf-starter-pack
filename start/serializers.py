a3f3ab98b3039b498c037ca958ce9d8f0d677a5d
Ulugbek J. <muskisgenius@gmail.com>
2025-04-19 09:33:43 +0500

    Api_shablon

---
+++ start/serializers.py
@@ -0,0 +1,30 @@
+from .models import Product, Review, Category, ProductViewHistory
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    avg_rating = serializers.FloatField(read_only=True, required=False)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'category', 'avg_rating']
+
+
+class ProductViewHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductViewHistory
+        fields = '__all__'
